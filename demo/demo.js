ccbd730775d046bc898935df3672f655f47841d8
Chrrrisss <chrisayodele10@gmail.com>
2022-08-11 21:34:39 +0100

    add bundle.js

---
+++ demo/demo.js
@@ -0,0 +1,5 @@
+const create_sample = require("..")
+
+const x = create_sample()
+
+document.body.innerHTML = `<h1> ${x} </h1>`
---
+++ src/index.js
@@ -0,0 +1,6 @@
+module.exports = create_sample
+
+function create_sample () {
+    const name = "loading wizard"
+    return name
+}
