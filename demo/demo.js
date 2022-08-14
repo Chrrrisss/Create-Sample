8c4d817885651af14e150a209e2c83c099bac107
Chrrrisss <chrisayodele10@gmail.com>
2022-08-15 00:02:26 +0100

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,20 @@
 const create_sample = require("..")
 
-const x = create_sample()
+const input1 = create_sample()
+const input2 = create_sample()
 
-document.body.append(x)
+const Title = "Crystals International Football Academy Form."
+const Subtitle = "Please fill out the forms"
+
+const page = document.createElement("div")
+page.innerHTML = `
+    <h1> ${Title} </h1>
+    <h2> ${Subtitle} </h2>
+    <h3>Enter your age:</h3>
+    <x></x>
+    <h4>Enter your year of birth:</h4>
+    <y></y>
+`
+page.querySelector("x").replaceWith(input1)
+page.querySelector("y").replaceWith(input2)
+document.body.append(page)
